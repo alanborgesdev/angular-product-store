0195cfc7aa4e32cc7b7a29feeebbf2e4d0e037d4
Alan Borges <alanborges05@gmail.com>
2025-07-14 08:42:22 -0300

    Initial commit

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+
+@Component({
+  selector: 'app-list',
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {
+  products: any[] = [];
+
+  productsService = inject(ProductsService);
+
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
